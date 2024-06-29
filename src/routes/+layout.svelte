a575b8191effab85ff2051bc55c98228b8b3ee44
nosh1114 <nao.shiba.public@gmail.com>
2024-06-29 23:16:58 +0900

    tailwindcssの導入

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <header class="flex items-center justify-between px-6 py-4 bg-black">
+    <h1 class="text-2xl font-bold text-white">Tech Blog</h1>
+    <nav class="flex gap-4">
+      <a href="/" class="text-white hover:text-gray-400">home</a>
+      <a href="/about" class="text-white hover:text-gray-400">About</a>
+    </nav>
+  </header>
+  
+  <slot />
+  
+  <footer class="bg-black py-2 text-center text-white">
+    <p>My Awesome Website!</p>
+  </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'], // ここを追加
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
