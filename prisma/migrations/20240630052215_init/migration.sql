8519a4b4d8432d403e909dbf4939707e0e6152fb
nosh1114 <nao.shiba.public@gmail.com>
2024-06-30 15:52:29 +0900

    教授の名前で検索できるようにする

---
+++ prisma/migrations/20240630052215_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `research_areas` on the `professors` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "professors" DROP COLUMN "research_areas";
--- src/lib/server/database/types.ts
+++ src/lib/server/database/types.ts
@@ -1,29 +1,24 @@
-import type { ColumnType } from 'kysely';
-
+import type { ColumnType } from "kysely";
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
-
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type Email = {
-  id: Generated<number>;
-  professorId: number;
-  subject: string;
-  body: string;
-  sentAt: Generated<Timestamp>;
+    id: Generated<number>;
+    professorId: number;
+    subject: string;
+    body: string;
+    sentAt: Generated<Timestamp>;
 };
-
 export type Professor = {
-  id: Generated<number>;
-  name: string;
-  department: string;
-  email: string;
-  profilePictureUrl: string;
-  researchAreas: string;
+    id: Generated<number>;
+    name: string;
+    department: string;
+    email: string;
+    profilePictureUrl: string;
 };
-
 export type DB = {
-  emails: Email;
-  professors: Professor;
+    emails: Email;
+    professors: Professor;
 };
