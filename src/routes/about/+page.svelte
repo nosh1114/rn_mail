350d3947b176495ee293a21c23be971c9c64863d
nosh1114 <nao.shiba.public@gmail.com>
2024-07-01 10:49:21 +0900

    aboutの補足

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,17 +5,19 @@
         <p class="text-gray-600 mb-6">
             RnMailsは、いちいちメールを探して回っていた学生のための簡単で使いやすいメール管理プラットフォームです。
         </p>
-        <h3 class="text-2xl font-semibold text-gray-700 mb-4">機能</h3>
+        <p class="text-gray-600 mb-6">
+            教授のメールアドレスを探すのが面倒だったり、メールの内容を毎回考えるのが面倒だったり、研究室の場所はどこだったかな、、、そんな学生のために作られました。
+        </p>
+        <h3 class="text-2xl font-semibold text-gray-700 mb-4">機能・使い方</h3>
         <ul class="list-disc list-inside text-gray-600 mb-6">
             <li>教授のプロフィールと連絡先情報のアクセス</li>
-            <li>Gmailのメールの効率化</li>
-            <li>モバイルデバイスの両方に対応したレスポンシブデザイン</li>
+            <li>Gmail直接メール送信画面に飛べる！</li>
             <li>教授の情報を名前・学科名・授業名から検索できる（実装予定）</li>
             <li>メールの内容テンプレートが使える（実装予定）</li>
         </ul>
         <h3 class="text-2xl font-semibold text-gray-700 mb-4">お問い合わせ</h3>
         <p class="text-gray-600">
-            ご質問やフィードバックがありましたら、<a href="mailto:s223683q@st.go.tuat.ac.jp" class="text-indigo-500 hover:underline">こちら</a>までお気軽にお問い合わせください。
+            ご質問やフィードバックがありましたら、運営の<a href="mailto:s223683q@st.go.tuat.ac.jp" class="text-indigo-500 hover:underline">こちら</a>までお気軽にお問い合わせください。
         </p>
     </section>
 </main>
